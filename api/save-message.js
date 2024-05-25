9add3b59be5b59f42f5f2be96e5d2cf3ad6d7ca1
Dipesh <dipeshch040313@gmail.com>
2024-05-25 16:59:20 +0530

    websocket2

--- api/save-message.js
+++ api/save-message.js
@@ -1,45 +1,10 @@
-const { Server } = require("ws");
-const saveMessage = require('./save-message');
+const fs = require('fs');
 
-const server = new Server({ noServer: true });
-const connections = new Set();
+function saveMessage(message) {
+    const messages = fs.readFileSync('./utils/messages.json');
+    const parsedMessages = JSON.parse(messages);
+    parsedMessages.push(message);
+    fs.writeFileSync('./utils/messages.json', JSON.stringify(parsedMessages));
+}
 
-server.on("connection", (socket) => {
-    connections.add(socket);
-
-    socket.on("message", (message) => {
-        const parsedMessage = JSON.parse(message);
-        const { type, content, sender } = parsedMessage;
-
-        // Broadcast to all connected clients except the sender
-        connections.forEach((conn) => {
-            if (conn !== socket && conn.readyState === WebSocket.OPEN) {
-                conn.send(JSON.stringify({ type, content, sender }));
-            }
-        });
-
-        // Save the message
-        saveMessage(parsedMessage);
-    });
-
-    socket.on("close", () => {
-        connections.delete(socket);
-    });
-});
-
-module.exports = (req, res) => {
-    if (res.socket.server.ws) {
-        res.end();
-        return;
-    }
-
-    res.socket.server.ws = server;
-
-    res.socket.server.on("upgrade", (req, socket, head) => {
-        server.handleUpgrade(req, socket, head, (ws) => {
-            server.emit("connection", ws, req);
-        });
-    });
-
-    res.end();
-};
+module.exports = saveMessage;
